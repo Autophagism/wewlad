9c52fb619ca97999e81aecfeb2a36a2e93dbdae4
AshesOverTheSeine <tysonsamuel93@gmail.com>
2016-02-09 12:59:52 -0800

    Create wewLAD.py

---
+++ wewLAD.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# A transgressive, modern re-interpretation of the classical "Fizz-buzz" computer science problem
+
+for i in range (1, 100):
+    if((i % 15) == 0):
+        print("W E W L A D")
+    elif((i % 3) == 0):
+        print("wew")
+    elif((i % 5) == 0):
+        print("lad")
+    else:
+        print(i)
